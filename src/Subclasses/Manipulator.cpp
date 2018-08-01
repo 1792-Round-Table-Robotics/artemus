d551cbd0e4a9e524af99a4d6c2c608a82b215a01
jmanrocks152 <jmanrocks152@gmail.com>
2018-07-31 19:06:14 -0500

    Moved Previously Committed Files Accordingly

---
+++ src/Subclasses/Manipulator.cpp
@@ -0,0 +1,15 @@
+/* Manipulator.cpp
+ * Created on: Jul 30, 2018
+ * Original Creator: Casey Jones
+ * Rio Firmware Version at Creation: 2018
+ * FRC Library Version at Creation: 2018
+ * Motor Controller Types in Use: TalonSRX, victor
+ * Motor Controller Firmware Version at Creation:
+ *
+ */
+
+#include <Subclasses/Manipulator.h>
+
+ManipulatorManager::ManipulatorManager() {
+
+}
---
+++ src/Subclasses/Manipulator.h
@@ -0,0 +1,21 @@
+/*
+ * Manipulator.h
+ *
+ *  Created on: Jul 30, 2018
+ *      Author: RTR
+ */
+
+#pragma once
+
+#ifndef SRC_SUBCLASSES_MANIPULATOR_H_
+#define SRC_SUBCLASSES_MANIPULATOR_H_
+
+class ManipulatorManager {
+
+public:
+	ManipulatorManager();
+};
+
+
+
+#endif /* SRC_SUBCLASSES_MANIPULATOR_H_ */
